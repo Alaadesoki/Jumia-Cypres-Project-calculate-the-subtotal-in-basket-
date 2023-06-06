8a20f95af6f62693047913e7709db7909a6b900e
Alaa Desoki <101141793+Alaadesoki@users.noreply.github.com>
2023-06-06 13:58:34 +0300

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,37 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    execTimeout: 12000,
+    taskTimeout: 12000,
+    viewportHeight: 1500,
+    viewportWidth: 1500,
+
+    retries: { "runMode": 0, "openMode": 0 },
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
+    video: false,
+    e2e: {
+       baseUrl: "https://www.jumia.com.eg/",
+        "specPattern": "cypress/e2e/**/*.{feature,features}",
+    setupNodeEvents(on, config) {
+        return require('./cypress/Plugins/index')(on, config),
+            on('task', {
+                log(message) {
+                    console.log(message)
+                    return null
+                },
+            })
+    },
+  },
+
+
+  
+  "reporter": "../node_modules/mochawesome/src/mochawesome.js",
+  "reporterOptions": {
+      "overwrite": false,
+      "html": false,
+      "json": true
+  }
+});
